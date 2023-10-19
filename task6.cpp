800d0480494d152fcecd25144a28ef34b55fa932
ZAIN MAHAR <144410914+zainMahar78@users.noreply.github.com>
2023-10-19 22:50:39 +0500

    Update task6.cpp

--- task6.cpp
+++ task6.cpp
@@ -3,12 +3,12 @@ using namespace std;
 void longest_duration(int h,int m);
 main(){
 	cout<<"Enter the number of hours: ";
-	int h;
-	cin>>h;
+	int h1;
+	cin>>h1;
 	cout<<"Enter the number of minutes: ";
-	int m;
-	cin>>m;
-	longest_duration(m,h);
+	int m1;
+	cin>>m1;
+	longest_duration(m1,h1);
 }
 void longest_duration(int m, int h){
 	float h_in_minutes;
