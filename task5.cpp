aa9a932f267e81a644897341dd98f0c8352aa633
ZAIN MAHAR <144410914+zainMahar78@users.noreply.github.com>
2023-10-19 22:49:26 +0500

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -3,12 +3,12 @@ using namespace std;
 void position(int a,int b);
 main(){
  cout <<"Enter your position: ";
-	int a;
-	cin>>a;
+	int a1;
+	cin>>a1;
  cout <<"Enter your friend's position: ";
-	int b;
-	cin>>b;
-	position(a,b);
+	int a2;
+	cin>>a2;
+	position(a1,a2);
 }
 void position(int a,int b){
 	int diff=b-a;
